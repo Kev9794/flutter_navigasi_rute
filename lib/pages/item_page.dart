4a0b0bd2eeee787a5b5997900fb0a2fdd93bd62a
Kev9794 <k.natanael88@gmail.com>
2022-12-12 23:16:46 +0700

    Navigasi dan Rute

---
+++ lib/pages/item_page.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+import '../models/item.dart';
+import 'package:flutter_navigasi_rute/pages/home_page.dart';
+
+class ItemPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final itemArgs = ModalRoute.of(context)!.settings.arguments as Item;
+
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          leading: IconButton(
+            icon: const Icon(Icons.arrow_back),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          ),
+          title: const Text('Shopping List-2041720091-Kevin Natanael Wijaya'),
+        ),
+        body: Center(
+            child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(itemArgs.name.toString()),
+            const Text(" with "),
+            Text(itemArgs.price.toString())
+          ],
+        )),
+      ),
+    );
+  }
+}
