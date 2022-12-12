c7a6bb5dfee6d43a324626f0ae5a88efb02820e7
Kev9794 <k.natanael88@gmail.com>
2022-12-12 23:45:02 +0700

    Tugas Modifikasi Halaman Item Lebih Menarik

--- lib/pages/item_page.dart
+++ lib/pages/item_page.dart
@@ -21,13 +21,42 @@ class ItemPage extends StatelessWidget {
           title: const Text('Shopping List-2041720091-Kevin Natanael Wijaya'),
         ),
         body: Center(
-            child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(itemArgs.name.toString()),
-            const Text(" with "),
-            Text(itemArgs.price.toString())
-          ],
+            child: SizedBox(
+          height: 250,
+          child: Container(
+            padding: const EdgeInsets.all(16.0),
+            decoration: BoxDecoration(
+              border:
+                  Border.all(width: 5, color: Color.fromARGB(255, 0, 211, 46)),
+              borderRadius: BorderRadius.all(Radius.circular(20)),
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  "Detail Barang :   ",
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Color.fromARGB(255, 83, 83, 83),
+                      fontSize: 30),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text("Nama Barang : "),
+                    Text(itemArgs.name.toString()),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text("Harga : Rp."),
+                    Text(itemArgs.price.toString()),
+                  ],
+                ),
+              ],
+            ),
+          ),
         )),
       ),
     );
